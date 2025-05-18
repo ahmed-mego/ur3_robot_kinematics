a8b9f1df071b4dacfa5fae5a7471c34c5e20e8d9
ahmed-mego <ahmedmego2555@gmail.com>
2025-05-18 21:31:02 +0300

    belal commit

--- robot/forward_kinematics.py
+++ robot/forward_kinematics.py
@@ -4,3 +4,4 @@ def forward_kinematics(joint_angles, dh_model):
     # Returns end-effector position and full transformation chain
     transforms = dh_model.get_joint_transforms(joint_angles)
     return transforms[-1]  # End-effector pose
+print("belal")
