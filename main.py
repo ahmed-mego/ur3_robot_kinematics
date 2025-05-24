bb3f0abc676acdc16cdafb632ba5dc4f2d82f038
Belyy-0 <belal2003gamal@gmail.com>
2025-05-24 04:36:41 +0300

    done

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from robot.forward_kinematics import ForwardKinematics
+from robot.dh_model import DHModel, load_dh_from_yaml
 import numpy as np
+from robot.inverse_kinematics import compute_inverse_kinematics
+
 
 def main():
-    config_path = "~/ur3_robot_kinematics/config/robot_config.yaml"
+    config_path = "C:/Users/Belal Gamal/Documents/ur3_robot_kinematics/config/robot_config.yaml"
+   
+
     fk_solver = ForwardKinematics(config_path)
 
     # Example joint angles in radians (UR3 has 6 DOF)
@@ -13,8 +18,17 @@ def main():
     end_effector_pose = transforms[5]
 
     position = end_effector_pose[:3, 3]
-
+    
     print("End-effector postion (x, y, z):", np.round(position,3))
+    target_position = np.array([
+    [1, 0, 0, 0.1],
+    [0, 1, 0, 0.3],
+    [0, 0, 1, 0.2],
+    [0, 0, 0, 1]
+    ])
+    ik_degrees = compute_inverse_kinematics(config_path, target_position)
+    print("IK solution :", ik_degrees)
+
 
     # for i, T in enumerate(transforms):
     #     print(f"Transform to joint {i+1}:")
--- robot/inverse_kinematics.py
+++ robot/inverse_kinematics.py
@@ -1,5 +1,26 @@
 import numpy as np
+from spatialmath import SE3
+from roboticstoolbox import DHRobot, RevoluteDH
+from robot.dh_model import load_dh_from_yaml  # Your existing function
 
-def inverse_kinematics(target_pose, dh_model, initial_guess):
-    # Use numerical IK (e.g., Jacobian-based or optimization)
-    pass
+
+def compute_inverse_kinematics(config_path, target_pose):
+
+
+    dh_params = load_dh_from_yaml(config_path)
+    links = [RevoluteDH(d=d, a=a, alpha=alpha) for theta, d, a, alpha in dh_params]
+    robot = DHRobot(links, name="our robot")
+
+    if isinstance(target_pose, np.ndarray) and target_pose.shape == (4, 4):
+        T = SE3(target_pose)
+    else:
+        raise ValueError("Target pose must be a 4x4 numpy array.")
+
+    result = robot.ikine_LM(T)
+
+    if result.success:
+        
+        degrees_whole = np.round(np.degrees(result.q))
+        return  degrees_whole
+    else:
+        print("Inverse kinematics failed to converge.")
